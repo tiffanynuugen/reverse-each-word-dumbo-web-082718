b78c9f228f82c6d2d5c488e2c898f83867186c6b
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-16 03:12:13 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,5 @@ def reverse_each_word(sentence)
   words_array = sentence.split
   new_array = []
   new_array = words_array.collect {|word| word.reverse }
-  new_array.join(" ")
+  new_array.join
 end
